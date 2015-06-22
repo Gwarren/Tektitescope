d27dbd5d7c0bc2fe0e4ae6a8b2e7886f530509cf
Grant M. Warren <gwarren1715@gmail.com>
2015-06-22 06:40:47 +0000

    updated through accounts ui and need to add submit posts template next

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,19 +1,7 @@
-var postsData = [{
-    title:'introducing Tektitescope', 
-    author:"Grant Warren", 
-    url:'http://sachagreif.com/introducing-telescope/'
-}, 
-    {
-        title:'Meteor', 
-        author:"Tom Coleman", 
-        url:'http://meteor.com'
-}, 
-    {
-        title: 'The meteor book', 
-        author:"Tom Coleman", 
-        url: 'http://themeteorbook.com'
-    }
-]; 
+
 Template.postsList.helpers ({
-    posts: postsData
+    posts: function() { 
+    
+        return Posts.find();
+    }
 });
